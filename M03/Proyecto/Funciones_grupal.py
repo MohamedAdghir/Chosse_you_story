5917521f8e03ad267131a862397a9407c429cd9e
MohamedAdghir <madghirettaibi.cf@iesesteveterradas.cat>
2026-01-08 19:23:56 +0100

    Add files via upload

--- M03/Proyecto/Funciones_grupal.py
+++ M03/Proyecto/Funciones_grupal.py
@@ -9,3 +9,21 @@ def getOpt(textOpts="",inputOptText="",rangeList=[],exceptions=[],dictionary={})
         else:
             print("Invalid Options")
 
+password = "Alolb8*"
+def checkPassword(password):
+    if len(password) not in range(8,12):
+        print("Invalid range")
+    elif not password:
+        print("hola")
+
+nombre = ("Holaaaa")
+def checkUser(user):
+    if len(user) not in range(6,10):
+        print("Invalid range")
+        return False
+    elif not user.isalpha():
+        print("Only numers and characters")
+        return False
+    return True
+
+checkUser(nombre)
